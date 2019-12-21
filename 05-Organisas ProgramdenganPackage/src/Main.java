31573603c6418a34238c974392671dd8bd719c00
dikyjakaria <55890687+dikyjakaria@users.noreply.github.com>
2019-12-21 22:16:58 +0700

    Add files via upload

---
+++ 05-Organisas ProgramdenganPackage/src/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Windows_10
+ * Nama     :M Diki Jakaria
+ * NIM      :A2.1900196
+ * Kelas    :TI-1C
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("hello world");
+    }
+    
+}
---
+++ 05-Organisas ProgramdenganPackage/src/OtongSurotong.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Windows_10
+ * Nama     :M Diki Jakaria
+ * NIM      :A2.1900196
+ * Kelas    :TI-1C
+ */
+public class OtongSurotong {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.print("Nama ku adalah Otong, teman si ucup");
+    }
+    
+}
